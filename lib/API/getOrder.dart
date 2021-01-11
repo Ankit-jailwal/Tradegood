1db12d4b4793181effee1ccc4be0f969baa5595e
Ankit Jailwal <jailwalanki@gmail.com>
2021-01-11 17:10:13 +0530

    Fixed bugs and changes in UI

---
+++ lib/API/getOrder.dart
@@ -0,0 +1,18 @@
+import 'dart:convert';
+import 'package:http/http.dart' as Http;
+import 'package:tradegood/API/authentication.dart';
+
+
+
+Future getOrder() async{
+  final String url = server + "/api/order/getOrderItems";
+  String res= await storage.read(key: 'jwt');
+  String token= "Bearer "+res;
+  print("token $token");
+  final response = await Http.get(url,
+    headers: {"Content-Type": "application/json","Authorization":"$token"},
+  );
+  final body=response.body;
+  final category=jsonDecode(body);
+  return category;
+}
