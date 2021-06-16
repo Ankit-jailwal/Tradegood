a96e62e14eb10397d9cd49eddce52ff2bb495d14
Ankit Jailwal <jailwalankit@gmail.com>
2021-06-16 12:31:04 +0530

    Migrated dependencies to null safety

--- lib/API/authentication.dart
+++ lib/API/authentication.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
-
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
 import 'package:connectivity/connectivity.dart';
